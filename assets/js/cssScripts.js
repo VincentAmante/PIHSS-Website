561796635137af8950c3a6824943f7d4e5e3d829
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2022-03-02 11:55:29 +0400

    Added experimental styling, improved header markup
    - Added some styling, it's definitely experimental and not final
    - Implemented some basic form of burger styling
    - Improved header markup, suitable for styling

---
+++ assets/js/cssScripts.js
@@ -0,0 +1,26 @@
+// This is code for handling the burger menu
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    // Toggles Nav
+    nav.classList.toggle("nav-active");
+
+    // Animates link arrival
+    navLinks.forEach((link, index) => {
+      if (link.style.animtion) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+      }
+    });
+
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
