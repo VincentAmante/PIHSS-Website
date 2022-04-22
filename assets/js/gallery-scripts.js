3e02d967719a094583665b4adeec91cae8dab885
Princelle Abbie Binobo <princellebinobo@gmail.com>
2022-04-23 01:18:21 +0800

    Updated Gallery, Contact Us pages
    General:
    - Adjusted code for banner and overview
    - Added placeholder breadcrumbs (to be replaced in the future)
    
    Gallery / Lightbox:
    - Inserted code for header and footer
    - Added custom scrollbar
    - Made zoom buttons functional
    - Attempted image drag/pan (Might need adjusting, behavior still a little awkward)
    
    Contact Us
    - Made page more responsive

--- assets/js/gallery-scripts.js
+++ assets/js/gallery-scripts.js
@@ -1,15 +1,18 @@
 /* QUERY SELECTORS */
 const lightboxEnabled = document.querySelectorAll(".lightbox-enabled");
 const lightboxArray = Array.from(lightboxEnabled);
-const lastImage = lightboxArray.length -1;
+const lastImage = lightboxArray.length - 1;
 
-// Lightbox header 
+// Lightbox header
 const lightboxSlideNum = document.querySelector("#slide-number");
+const lightboxBtnZoomIn = document.querySelector("#zoom-in");
+const lightboxBtnZoomOut = document.querySelector("#zoom-out");
 const lightboxBtnClose = document.querySelector("#close");
 const lightboxBtnDownload = document.querySelector("#download");
 
 // Lightbox content
 const lightboxContainer = document.querySelector(".modal");
+const lightboxSlide = document.querySelector(".lightbox-slide");
 const lightboxImage = document.querySelector(".lightbox-image");
 
 // Previous, Next buttons
@@ -18,23 +21,30 @@ const lightboxBtnPrev = document.querySelector("#prev");
 const lightboxBtnNext = document.querySelector("#next");
 
 let activeImage;
+let changeZoom = 1;
 
 /* FUNCTIONS */
 const showLightBox = () => {
   lightboxContainer.classList.add("active");
   document.querySelector("body").style.overflow = "hidden";
+  document.querySelector("#header").style.display = "none";
 };
 
 const hideLightBox = () => {
   lightboxContainer.classList.remove("active");
   document.querySelector("body").style.overflow = "auto";
+  document.querySelector("#header").style.display = "flex";
 };
 
 const setActiveImage = (image) => {
+  lightboxImage.style.transform = "scale(1)";
+  lightboxSlide.style.transform = "none";
+  changeZoom = 1;
+
   lightboxImage.src = image.dataset.imagesrc;
   activeImage = lightboxArray.indexOf(image);
 
-  lightboxSlideNum.innerHTML = (activeImage + 1) + " / " + lightboxArray.length;
+  lightboxSlideNum.innerHTML = activeImage + 1 + " / " + lightboxArray.length;
 };
 
 const transitionSlidePrev = () => {
@@ -44,9 +54,9 @@ const transitionSlidePrev = () => {
 };
 
 const transitionSlideNext = () => {
-    activeImage === lastImage
-      ? setActiveImage(lightboxArray[0])
-      : setActiveImage(lightboxArray[activeImage + 1]);
+  activeImage === lastImage
+    ? setActiveImage(lightboxArray[0])
+    : setActiveImage(lightboxArray[activeImage + 1]);
 };
 
 const transitionSlideHandler = (moveItem) => {
@@ -55,6 +65,11 @@ const transitionSlideHandler = (moveItem) => {
 
 const downloadSlide = (image) => {
   lightboxBtnDownload.href = lightboxArray[image].src;
+};
+
+const zoomSlide = (changeZoom) => {
+  console.log(changeZoom);
+  lightboxImage.style.transform = "scale(" + changeZoom + ")";
 };
 
 /* EVENT LISTENERS */
@@ -66,12 +81,6 @@ lightboxEnabled.forEach((image) => {
   });
 });
 
-[lightboxContainer, lightboxBtnClose].forEach((btn) => {
-  btn.addEventListener("click", () => {
-  hideLightBox()
-  });
-});
-
 lightboxBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.stopPropagation();
@@ -79,21 +88,94 @@ lightboxBtns.forEach((btn) => {
   });
 });
 
+[lightboxBtnZoomIn, lightboxBtnZoomOut].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    changeZoom = Math.round(changeZoom * 10) / 10;
+
+    if (e.currentTarget.id.includes("zoom-in")) {
+      console.log("zoom in");
+      if (changeZoom < 3) {
+        changeZoom += 0.2;
+      }
+    } else if (e.currentTarget.id.includes("zoom-out")) {
+      console.log("zoom out");
+      if (changeZoom > 0.2) {
+        changeZoom -= 0.2;
+      }
+    }
+
+    zoomSlide(changeZoom);
+  });
+});
+
+lightboxBtnClose.addEventListener("click", () => {
+  hideLightBox();
+});
+
 lightboxBtnDownload.addEventListener("click", () => {
   downloadSlide(activeImage);
 });
 
 // Keyboard input
-window.addEventListener('keydown', (e) => {
-  console.log(e.key)
+window.addEventListener("keydown", (e) => {
+  console.log(e.key);
 
-  if(e.key.includes('Left') || e.key.includes('Right')) {
+  if (e.key.includes("Left") || e.key.includes("Right")) {
     e.preventDefault();
     transitionSlideHandler(e.key.toLowerCase());
   }
 
-  if(e.key.includes('Esc')) {
+  if (e.key.includes("Esc")) {
     hideLightBox();
   }
-})
+});
 
+// -----------------------------
+// IMAGE PANNING
+// TODO: Fix awkward behavior
+// TODO: Organise code
+
+// Distance between last and new position
+var state = { distX: 0, distY: 0 };
+
+function onDown(e) {
+  // Stop bubbling, this is important to avoid
+  // unexpected behaviours on mobile browsers:
+  e.preventDefault();
+
+  // Get the correct event source regardless the device:
+  var evt = e.type === "touchstart" ? e.changedTouches[0] : e;
+
+  // Get the distance of the x/y
+  state.distX = Math.abs(lightboxSlide.offsetLeft - evt.clientX);
+  state.distY = Math.abs(lightboxSlide.offsetTop - evt.clientY);
+
+  // Disable pointer events in the circle to avoid
+  // a bug whenever it's moving.
+  lightboxSlide.style.pointerEvents = "none";
+}
+function onUp(e) {
+  // Re-enable the "pointerEvents" in the circle element.
+  // If this is not enabled, then the element won't move.
+  lightboxSlide.style.pointerEvents = "initial";
+}
+function onMove(e) {
+  // Update the position x/y
+  if (lightboxSlide.style.pointerEvents === "none") {
+    // Get the correct event source regardless the device:
+    var evt = e.type === "touchmove" ? e.changedTouches[0] : e;
+
+    // Update top/left directly in the dom element:
+    lightboxSlide.style.transform = `translate(${
+      evt.clientX - state.distX
+    }px, ${evt.clientY - state.distY}px)`;
+  }
+}
+
+// FOR MOUSE DEVICES:
+lightboxSlide.addEventListener("mousedown", onDown);
+lightboxContainer.addEventListener("mousemove", onMove);
+lightboxContainer.addEventListener("mouseup", onUp);
+
+// FOR TOUCH DEVICES:
+lightboxContainer.addEventListener("touchmove", onMove);
