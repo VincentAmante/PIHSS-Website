806b0db94e27b6eba685f2fd436cb4eefd820dd9
Princelle Abbie Binobo <princellebinobo@gmail.com>
2022-05-31 00:21:57 +0400

    Contact Form Implementation

--- assets/php/contact-submit.php
+++ assets/php/contact-submit.php
@@ -11,11 +11,13 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
 
     $mailTo = "princellebinobo@gmail.com"; // TODO: Change to PIHSS email
-    $headers = "From: ".$emailFrom;
+    $headers = "From: " . $emailFrom;
 
     // TODO: Confirm custom message
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsent");
+    mail($mailTo, $subject, $txt, $headers)
+        or die("Error!");
+
+    // header("Location: index.php?mailsent");
 }
