a090aaa4982ddf63874e286248cfa7655e192ff1
Princelle Abbie Binobo <princellebinobo@gmail.com>
2022-05-30 22:22:10 +0400

    Gallery adjustments, Contact form attempt
    - Returned gallery images back to stock photos
    - Made attempt on Contact form functionality (WIP)

---
+++ assets/php/contact-submit.php
@@ -0,0 +1,21 @@
+<?php
+
+// !WIP
+// Note: Can only be tested if the site is online
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $emailFrom = $_POST['email'];
+    $tel = $_POST['tel'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "princellebinobo@gmail.com"; // TODO: Change to PIHSS email
+    $headers = "From: ".$emailFrom;
+
+    // TODO: Confirm custom message
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsent");
+}
