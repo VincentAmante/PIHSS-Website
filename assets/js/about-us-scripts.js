bb3963713a0de908ebc09b6f65edcee33f33362b
Princelle Abbie Binobo <princellebinobo@gmail.com>
2022-07-12 09:40:22 +0400

    About Us page update
    - Adjusted page content according to redesign
    - Implemented 'List of Principals' timeline

--- assets/js/about-us-scripts.js
+++ assets/js/about-us-scripts.js
@@ -1,21 +1,21 @@
 // Code for the tabs
 const tabs = document.querySelectorAll('input[name="about-us-group"]');
 
-let selectedTab = "why-pihss";
+let selectedTab = "values";
 
-const tab_container = document.getElementById('about-us-tabs');
+const tab_container = document.getElementById("about-us-tabs");
 tab_container.addEventListener("click", () => {
-    for (const btn of tabs){
-        if (btn.checked){
-            document.getElementById(selectedTab + "-content").classList.remove("active");
-            document.getElementById(selectedTab + "-img").classList.remove("active");
-            document.getElementById("tab-" + selectedTab).classList.remove("active");
+	for (const btn of tabs) {
+		if (btn.checked) {
+			document.getElementById(selectedTab + "-content").classList.remove("active");
+			document.getElementById(selectedTab + "-img").classList.remove("active");
+			document.getElementById("tab-" + selectedTab).classList.remove("active");
 
-            const value = btn.getAttribute("value");
-            document.getElementById(value + "-content").classList.add("active");
-            document.getElementById(value + "-img").classList.add("active");
-            document.getElementById("tab-" + value).classList.add("active");
-            selectedTab = value;    
-        }
-    }
+			const value = btn.getAttribute("value");
+			document.getElementById(value + "-content").classList.add("active");
+			document.getElementById(value + "-img").classList.add("active");
+			document.getElementById("tab-" + value).classList.add("active");
+			selectedTab = value;
+		}
+	}
 });
