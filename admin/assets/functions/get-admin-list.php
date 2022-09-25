2c0920fcd568c400f0b64866b5e758881a3468c5
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2022-09-25 09:27:20 +0400

    Quick fix for admin accounts

--- admin/assets/functions/get-admin-list.php
+++ admin/assets/functions/get-admin-list.php
@@ -13,7 +13,7 @@
     while ($data = $admins->fetch_assoc()):?>
     <div class="row-item">
         <div class="title"><?php echo $data['user']?></div>
-        <div class="date"><?php echo ($data['email'])? 'Secondary' : 'Primary'; ?></div>
+        <div class="date"><?php echo ($data['isPrimary'])? 'Primary' : 'Secondary'; ?></div>
         <a class="delete delete-account" href="<?php echo "?delete-account=" . $data['ID']?>">                      
             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
