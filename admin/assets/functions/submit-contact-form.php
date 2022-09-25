333a045d32ff3d0d95ebec0d0ae1d3b508975f3c
Princelle Abbie Binobo <princellebinobo@gmail.com>
2022-09-25 22:00:38 +0400

    Updated submit-contact-form.php, edited images

--- admin/assets/functions/submit-contact-form.php
+++ admin/assets/functions/submit-contact-form.php
@@ -1,25 +1,39 @@
 <?php
+if (
+    !isset($_POST['name'])
+    || !isset($_POST['email'])
+    || !isset($_POST['tel'])
+    || !isset($_POST['subject'])
+    || !isset($_POST['message'])
+) {
+
+    // Returns to page  
+    $referer = '/contact-us.php?error=true';
+    header("Location: $referer");
+    exit();
+}
 
 if (isset($_POST['submit'])) {
-    require_once './config.php';
 
     // Form values
-    $name = $_POST['name'];
-    $emailFrom = $_POST['email'];
-    $tel = $_POST['tel'];
+    $name = htmlspecialchars($_POST['name']);
+    $emailFrom = htmlspecialchars($_POST['email']);
+    $tel = htmlspecialchars($_POST['tel']);
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
+    // Mail Details
     $mailTo = "info@pihss-shj.com";
     $headers = "From: " . $emailFrom;
 
     // Message template 
-    $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
+    $txt = "Name: " . $name . "\nMobile Number: " . $tel . "\n\nMessage: " . $message;
 
+    // Sends Message
     mail($mailTo, $subject, $txt, $headers)
         or die("Error!");
 
     // Returns to page
-    $referer = $_SERVER['HTTP_REFERER'];
+    $referer = '/contact-us.php?error=true';
     header("Location: $referer");
 }
